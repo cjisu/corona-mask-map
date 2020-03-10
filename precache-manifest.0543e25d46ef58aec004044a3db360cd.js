fbf130ea4d2c0c4579611ab17ccb36e6db9c3ddf
Sunset(최지수) <sunset.choi@bunjang.co.kr>
2020-03-11 01:42:27 +0900

    Updates

---
+++ precache-manifest.0543e25d46ef58aec004044a3db360cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3af674c89881b48be5772f8233cde1b",
+    "url": "/corona-mask-map/index.html"
+  },
+  {
+    "revision": "3a3b5bda70965356bbe2",
+    "url": "/corona-mask-map/static/js/2.ad77f574.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/corona-mask-map/static/js/2.ad77f574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7cf9b15db101e65dfee",
+    "url": "/corona-mask-map/static/js/main.dabaad6c.chunk.js"
+  },
+  {
+    "revision": "11d0d716d4faab155827",
+    "url": "/corona-mask-map/static/js/runtime-main.c3a02934.js"
+  }
+]);
